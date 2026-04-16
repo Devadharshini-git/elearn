ec580eb5bcb13da5f3350c344be728046af87903
Devadharshini-git <deva95069@gmail.com>
2026-04-16 22:09:08 +0530

    resolved conflict

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 $host = "db";
-$user = "root";   // Default XAMPP username
-$pass = "";       // No password in XAMPP
-$db = "learning_db"; // Database name
+$user = "root";
+$password = "root";
+$dbname = "learning_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
